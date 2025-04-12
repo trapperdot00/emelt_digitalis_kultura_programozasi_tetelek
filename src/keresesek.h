0a6f44aae9a58b7aa643346b770376e9a809aaf8
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-04-12 03:35:22 +0200

    Bináris keresés implementációja és tesztprogramja

--- src/keresesek.h
+++ src/keresesek.h
@@ -11,7 +11,7 @@ namespace csrb {
 // A felhasználónak ügyelnie kell az iterátorszakasz validitására
 // Visszatérési érték:
 // 	Az adatsorban megtalált elemre mutató iterátor,
-// 	vagy end paraméter (adatsor utolsó utáni elemre mutató iterátor),
+// 	vagy end paraméter (adatsor utolsó utáni elemére mutató iterátor),
 // 	ha nem volt találat
 template <class InputIt, class T>
 InputIt linearis_kereses
@@ -19,6 +19,35 @@ InputIt linearis_kereses
 	while (beg != end && *beg != val)
 		++beg;
 	return beg;
+}
+
+// Adott értékű elem keresése és a rendezett adatsorban elfoglalt
+// helyének megadása
+// Paraméterek:
+// 	beg: Az adatsor első elemére mutató iterátor
+// 	end: Az adatsor utolsó utáni elemére mutató iterátor
+// 	val: A keresett érték
+// A felhasználónak ügyelnie kell az iterátorszakasz validitására,
+// és az adatsor rendezettségére
+// Visszatérési érték:
+// 	Az adatsorban megtalált elemre mutató iterátor,
+// 	vagy end paraméter (adatsor utolsó utáni elemére mutató iterátor),
+// 	ha nem volt találat
+template <class RandomIt, class T>
+RandomIt binaris_kereses
+(RandomIt beg, RandomIt end, const T& val) {
+	RandomIt last = end;
+	RandomIt mid = beg + (last - beg) / 2;
+	while (mid != last && *mid != val) {
+		if (*mid < val)
+			beg = mid + 1;
+		else
+			last = mid;
+		mid = beg + (last - beg) / 2;
+	}
+	if (mid != end && *mid == val)
+		return mid;
+	return end;
 }
 
 }	// csrb namespace vége
